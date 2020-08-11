ea25b90dd66eb34feb067b779885fbfd3070a18c
Ankan Sikdar <ankansikdar@gmail.com>
2020-08-11 20:50:17 +0530

    Added Theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
-    );
+        home: InputPage(),
+        theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21),
+        ));
   }
 }
 
